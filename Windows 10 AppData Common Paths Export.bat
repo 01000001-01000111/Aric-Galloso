bcf7378681a6ebcb486892c018e51c3a1ade4823
01000001-01000111 <48495702+01000001-01000111@users.noreply.github.com>
2021-10-01 15:02:23 -0400

    Update Windows 10 AppData Common Paths Export.bat

--- Windows 10 AppData Common Paths Export.bat
+++ Windows 10 AppData Common Paths Export.bat
@@ -1,4 +1,4 @@
-﻿:: This copies common files found in AppData under a Windows 10 user profile, to your flash drive.
+﻿:: This batch file copies common files found in AppData under a Windows 10 user profile, to your flash drive.
 :: Edit the two lines below to reflect the drive letter of your flash drive and the user that we are pulling data from.
 :: Run as the user, not as admin.
 :: Created by Aric Galloso 2/15/2021
